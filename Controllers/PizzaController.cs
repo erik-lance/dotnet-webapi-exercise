5e607605e893f25a200addf03c692645391777d4
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2023-07-10 12:24:37 +0800

    Added PUT for modifying a pizza

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -37,7 +37,16 @@ public class PizzaController : ControllerBase
     [HttpPut("{id}")]
     public IActionResult Update(int id, Pizza pizza)
     {
-        // This code will update the pizza and return a result
+        if (id != pizza.Id)
+            return BadRequest();
+            
+        var existingPizza = PizzaService.Get(id);
+        if(existingPizza is null)
+            return NotFound();
+    
+        PizzaService.Update(pizza);           
+    
+        return NoContent();
     }
 
     [HttpDelete("{id}")]
